7b8a30d4ba75ba5741ea1ea233ef9a5efc504310
Horea Constandache <fortech.constandache@extaccount.com>
2019-08-10 13:31:29 +0300

    cleaded code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import './App.css';
 import NavRouter from './routing';
-import Navigation from './containers/navigation/Navigation';
 
 class App extends React.Component<object, object> {
-  
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      clicked: false,
-    }
-  }
 
   public render() {
 
